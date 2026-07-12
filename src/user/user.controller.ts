73e156e197e92d82a1b4f8713cc15229a5defa07
Ifajul Islam <135067157+ifajulislam@users.noreply.github.com>
2026-07-12 16:10:45 +0600

    Added user logic

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  // All of the get queries will be here
+  @Get()
+  getUser(@Query('name') name: string) {
+    const users = [
+      { id: 1, name: 'Abul' },
+      { id: 2, name: 'Babul' },
+    ];
+
+    if (name) {
+      return users.filter((user) =>
+        user.name.toLowerCase().includes(name.toLowerCase()),
+      );
+    }
+
+    return users;
+  }
+}
