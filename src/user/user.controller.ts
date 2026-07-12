3b374226c43d3d3799c42bcb4a3ad1472a58541a
Ifajul Islam <135067157+ifajulislam@users.noreply.github.com>
2026-07-12 17:35:18 +0600

    Added dtos and new api methods

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
   // All of the get queries will be here
   @Get()
-  getUser(@Query('name') name: string) {
+  getAllUser(@Query('name') name: string) {
     const users = [
       { id: 1, name: 'Abul' },
       { id: 2, name: 'Babul' },
@@ -17,5 +19,31 @@ export class UserController {
     }
 
     return users;
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    const users = [
+      { id: 1, name: 'Abul' },
+      { id: 2, name: 'Babul' },
+    ];
+
+    if (id) {
+      return users.filter((user) =>
+        user.name.toLowerCase().includes(id.toLowerCase()),
+      );
+    }
+
+    return users;
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return { data: createUserDto, message: 'User created successfully' };
+  }
+
+  @Put()
+  updateUser(@Body() updateUserDto: UpdateUserDto) {
+    return { data: updateUserDto, message: 'User updated successfully' };
   }
 }
